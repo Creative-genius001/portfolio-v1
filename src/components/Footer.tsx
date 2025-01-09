f3765e98fb9990056193e94264ac463b5b9aac75
unknown <ovieighosuakpo@gmail.com>
2025-01-09 09:51:00 +0100

    fixed mobile twitter link broken

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,7 +6,7 @@ const Footer = () => {
     <footer className="w-full pt-[10rem] pb-8 flex flex-col justify-center items-center">
         <div className='footer-icons flex justify-evenly mb-4 w-[20%]'>
             <a href='https://github.com/Creative-genius001'><FiGithub className='icon text-slate-300 lg:text-[1.3rem] sm:text-[1.1rem] hover:text-tech-blue transition-all duration-300' /></a>
-            <a href='https://twitter.com/ovieDgenius'><FiTwitter className='icon text-slate-300 lg:text-[1.3rem] sm:text-[1.1rem] hover:text-tech-blue transition-all duration-300 ' /></a>
+            <a href='https://x.com/kingovie_007'><FiTwitter className='icon text-slate-300 lg:text-[1.3rem] sm:text-[1.1rem] hover:text-tech-blue transition-all duration-300 ' /></a>
             <a href='https://www.linkedin.com/in/ovie-ighosuakpo/'><FiLinkedin className='icon text-slate-300 lg:text-[1.3rem] sm:text-[1.1rem] hover:text-tech-blue transition-all duration-300'/></a>
         </div>
         <span className="code  text-center text-slate-300 lg:text-sm sm:text-xs ">Designed and built by Ovie Ighosuakpo</span>
